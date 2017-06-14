72e05ba49facda9bb0c4f1f91ba923cbab68f3e3
Mac Heller-Ogden <mac@machellerogden.com>
2017-06-14 15:48:41 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const os = require('os');
+const bytes = require('bytes');
+const Promise = require('bluebird');
+
+function memory() {
+    return Promise.resolve(bytes(os.freemem()));
+}
+memory.label = 'Free Memory';
+
+module.exports = {
+    memory
+};
